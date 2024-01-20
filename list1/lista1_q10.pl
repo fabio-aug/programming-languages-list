cad45736f48321980a1a30ec2d34155c972072ad
Luana Assis Silva <88562681+luanaassis@users.noreply.github.com>
2024-01-20 09:55:12 -0300

    Update lista1_q10.pl

--- list1/lista1_q10.pl
+++ list1/lista1_q10.pl
@@ -9,3 +9,7 @@ gosta(zumira,vinho).
 gosta(pedro,X) :- gosta(X,vinho).
 gosta(maria,X) :- gosta(X,vinho), gosta(X,chocolate).
 gosta(maria,X) :- gosta(X,pedro), gosta(X,chocolate).
+
+-----------------------------------------
+
+gosta(maria,X)
